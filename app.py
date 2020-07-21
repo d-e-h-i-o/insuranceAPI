379616393fec99eaa45fc3f0ff29938e6661ea51
d-e-h-i-o <niklas.dehio@gmail.com>
2020-07-21 20:20:24 +0200

    more setup (database, migrations)

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import os
 
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from models import User
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello World!!'
 
 
 if __name__ == '__main__':
