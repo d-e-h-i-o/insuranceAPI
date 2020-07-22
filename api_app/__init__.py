c5e4ba5d01fab13b4683a8eaea010f284a077227
d-e-h-i-o <niklas.dehio@gmail.com>
2020-07-23 00:59:50 +0200

    gunicord setup

--- api_app/__init__.py
+++ api_app/__init__.py
@@ -17,7 +17,7 @@ def create_app(test_config=None):
         # load the instance config, if it exists, when not testing
         app.config.from_pyfile('../config.py')
         #app.config['DATABASE'] = 'postgresql:///insuranceapi_dev'
-        #app.config['DATABASE'] = os.environ
+        app.config['DATABASE'] = os.environ['DATABASE_URL']
     else:
         # load the test config if passed in
         app.config.from_mapping(test_config)
